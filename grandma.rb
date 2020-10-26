7ef037687fb3dfdbe1e508a4f51c9851824ef691
Will Studabaker <will.studabaker@gmail.com>
2020-10-26 20:59:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
   if phrase.upcase != phrase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "NO, NOT SINCE 1938!"
